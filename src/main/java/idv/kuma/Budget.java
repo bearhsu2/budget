63354b6411b247ca42e62dc70c857faaaa69176b
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-04 22:43:48 +0800

    clear up

--- src/main/java/idv/kuma/Budget.java
+++ src/main/java/idv/kuma/Budget.java
@@ -3,27 +3,7 @@ package idv.kuma;
 public class Budget {
 
     private String yearMonth;
-
-    public Budget(String yearMonth, int amount) {
-        this.yearMonth = yearMonth;
-        this.amount = amount;
-    }
-
     private int amount;
 
-    public String getYearMonth() {
-        return yearMonth;
-    }
 
-    public void setYearMonth(String yearMonth) {
-        this.yearMonth = yearMonth;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
 }
