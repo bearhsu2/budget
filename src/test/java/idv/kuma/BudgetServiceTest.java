b8402e6b21d7d1dcd8d9bc4475f97e950fedf515
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-05 22:37:16 +0800

    refactor test

--- src/test/java/idv/kuma/BudgetServiceTest.java
+++ src/test/java/idv/kuma/BudgetServiceTest.java
@@ -1,6 +1,7 @@
 package idv.kuma;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -14,25 +15,33 @@ import java.util.Arrays;
 
 public class BudgetServiceTest {
 
+    private BudgetService budgetService;
+    private BudgetRepo mockedBudgetRepo;
+
+    @Before
+    public void setUp() throws Exception {
+
+        mockedBudgetRepo = Mockito.mock(BudgetRepo.class);
+        budgetService = new BudgetService(mockedBudgetRepo);
+    }
+
     @Test
     public void start_end_both_in_budget() {
 
-        BudgetRepo mockedBudgetRepo = Mockito.mock(BudgetRepo.class);
+        prepareBudgetRepo(new Budget("201904", 30));
+
+
+        runAndCheck(1D, LocalDate.of(2019, 4, 1), LocalDate.of(2019, 4, 1));
+
+    }
+
+    private void runAndCheck(double expected, LocalDate start, LocalDate end) {
+        Assert.assertEquals(expected, budgetService.query(start, end), 0.001);
+    }
+
+    private void prepareBudgetRepo(Budget... budgets) {
         Mockito.when(mockedBudgetRepo.getAll()).thenReturn(
-                Arrays.asList(
-                        new Budget("201904", 30)
-                )
+                Arrays.asList(budgets)
         );
-
-        BudgetService budgetService = new BudgetService(mockedBudgetRepo);
-
-        double result = budgetService.query(
-                LocalDate.of(2019, 4, 1),
-                LocalDate.of(2019, 4, 1)
-
-        );
-
-        Assert.assertEquals(1D, result, 0.001);
-
     }
 }
